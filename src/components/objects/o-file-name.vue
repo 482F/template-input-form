71c9bac17b70c69d3bf33d8f5b35fd3057a43ab4
482F <482F.git.mail@482f.net>
2022-04-07 00:00:00 +0900

    ファイル名を表示できるように

---
+++ src/components/objects/o-file-name.vue
@@ -0,0 +1,28 @@
+<template>
+  <a-text-field :model-value="value" @update:model-value="update" />
+</template>
+
+<script>
+import ObjectsMixin from './objects.mixin.js'
+
+export default {
+  name: 'o-text',
+  mixins: [ObjectsMixin],
+  data() {
+    return {
+      value: '',
+    }
+  },
+  methods: {
+    update(newValue) {
+      this.value = newValue
+      this.$emit('update:result', { fileName: newValue })
+    },
+  },
+  mounted() {
+    this.update(this.object.default ?? '')
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
