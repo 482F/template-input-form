00eef41ce6dd45ef0448eaf0f7c7520efa0be5a8
482F <482F.git.mail@482f.net>
2022-04-07 00:00:00 +0900

    select 型のフォームを表示できるように

---
+++ src/components/objects/o-select.vue
@@ -0,0 +1,50 @@
+<template>
+  <a-select
+    class="o-select"
+    ref="aSelect"
+    :model-value="value"
+    :items="items"
+    @update:model-value="update"
+  />
+</template>
+
+<script>
+import ObjectsMixin from './objects.mixin.js'
+
+export default {
+  name: 'o-select',
+  mixins: [ObjectsMixin],
+  data() {
+    return {
+      value: '',
+    }
+  },
+  computed: {
+    items() {
+      return (this.object.selects ?? '')
+        .replaceAll(/^\[|\]$/g, '')
+        .split(',')
+        .map((value) => value.replaceAll(/^\s+|\s+$/g, ''))
+    },
+  },
+  methods: {
+    focus() {
+      this.$refs.aSelect.focus()
+    },
+    update(newValue) {
+      this.value = newValue
+      this.$emit('update:result', { body: newValue })
+    },
+  },
+  mounted() {
+    this.update(this.object.default ?? '')
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.o-select {
+  width: 16rem;
+  padding: 0.5rem 0;
+}
+</style>
