a1f5964843ce1522679344c4f55407ea3e121784
482F <482F.git.mail@482f.net>
2022-04-07 00:00:00 +0900

    半角スペースが表示できるように

--- src/components/objects/o-plain.vue
+++ src/components/objects/o-plain.vue
@@ -1,6 +1,9 @@
 <template>
   <span>
-    {{ object.value }}
+    <template v-for="(value, i) of nonBreakedValues" :key="i">
+      <span v-if="value.type === 'plain'">{{ value.value }}</span>
+      <span v-else-if="value.type === 'nbsp'" v-html="value.value" />
+    </template>
   </span>
 </template>
 
@@ -12,6 +15,30 @@ export default {
   mixins: [ObjectsMixin],
   mounted() {
     this.$emit('update:result', { body: this.object.value })
+  },
+  computed: {
+    nonBreakedValues() {
+      const values = []
+      const value = this.object.value ?? ''
+      const matches = value.matchAll(/\s+/g)
+      let processed = 0
+      for (const match of matches) {
+        values.push({
+          type: 'plain',
+          value: value.slice(processed, match.index),
+        })
+        values.push({
+          type: 'nbsp',
+          value: Array(match[0].length).fill('&nbsp;').join(''),
+        })
+        processed += match.index + match.length
+      }
+      values.push({
+        type: 'plain',
+        value: value.slice(processed, value.length),
+      })
+      return values
+    },
   },
 }
 </script>
