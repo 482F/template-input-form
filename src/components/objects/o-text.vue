b7377362e2da20f62df7054d49566031dc65485f
482F <482F.git.mail@482f.net>
2022-04-07 00:00:00 +0900

    テキスト入力欄を追加

---
+++ src/components/objects/o-text.vue
@@ -0,0 +1,32 @@
+<template>
+  <a-text-field class="o-text" :model-value="value" @update:model-value="update" />
+</template>
+
+<script>
+import ObjectsMixin from './objects.mixin.js'
+
+export default {
+  name: 'o-text',
+  mixins: [ObjectsMixin],
+  data() {
+    return {
+      value: '',
+    }
+  },
+  methods: {
+    update(newValue) {
+      this.value = newValue
+      this.$emit('update:result', { body: newValue })
+    },
+  },
+  mounted() {
+    this.update(this.object.default ?? '')
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.o-text {
+  width: 16rem;
+}
+</style>
