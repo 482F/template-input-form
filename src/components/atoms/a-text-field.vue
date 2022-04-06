ae269b4bdcbc675eddbc4b4a8896a013f986199f
482F <482F.git.mail@482f.net>
2022-04-07 00:00:00 +0900

    text-field コンポーネントを追加

---
+++ src/components/atoms/a-text-field.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-text-field class="a-text-field" variant="outlined" density="compact" />
+</template>
+
+<script>
+export default {
+  name: 'a-text-field',
+}
+</script>
+
+<style lang="scss" scoped>
+.a-text-field {
+  > ::v-deep(.v-input__details) {
+    display: none;
+  }
+}
+</style>
