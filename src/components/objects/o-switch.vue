98ca2b65cd367b02214a1691f5689d5f3b073dd6
482F <482F.git.mail@482f.net>
2022-04-07 00:00:00 +0900

    type: switch を追加

---
+++ src/components/objects/o-switch.vue
@@ -0,0 +1,47 @@
+<template>
+  <a-switch
+    class="o-switch"
+    ref="aSwitch"
+    :model-value="value"
+    :label="body"
+    @update:model-value="update"
+  />
+</template>
+
+<script>
+import ObjectsMixin from './objects.mixin.js'
+
+export default {
+  name: 'o-switch',
+  mixins: [ObjectsMixin],
+  data() {
+    return {
+      value: '',
+    }
+  },
+  computed: {
+    body() {
+      return this.object[this.value] ?? this.value
+    },
+  },
+  methods: {
+    focus() {
+      this.$refs.aSwitch.focus()
+    },
+    update(newValue) {
+      this.value =
+        typeof newValue === 'boolean'
+          ? newValue
+          : newValue === 'true'
+          ? true
+          : false
+      this.$emit('update:result', { body: this.body })
+    },
+  },
+  mounted() {
+    this.update(this.object.default ?? '')
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
