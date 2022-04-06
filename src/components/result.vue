22b6cc3a6e26383df77ce10a989b5b8462a4e6d5
482F <482F.git.mail@482f.net>
2022-04-07 00:00:00 +0900

    結果を表示できるように

---
+++ src/components/result.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="result">
+    <div class="controls">
+      ファイル名: <span class="filename">{{ result.fileName?.[0] ?? '' }}</span>
+    </div>
+    <a-textarea
+      class="body"
+      :model-value="result.body?.join?.('') ?? ''"
+      readonly
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'result',
+  props: {
+    result: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.result {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  .body {
+    flex-grow: 1;
+  }
+}
+</style>
