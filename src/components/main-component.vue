45350fa3f3a7fbfda925e28e737b660a205cebea
482F <0ryonryoko0@gmail.com>
2022-04-09 06:36:55 +0900

    外側のコンポーネントを作成

---
+++ src/components/main-component.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="main-component">main-component</div>
+</template>
+
+<script>
+export default {
+  name: 'main-component',
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
