0b6e88adf345e7e6cf1a16e7351503007c2321e1
482F <0ryonryoko0@gmail.com>
2022-04-09 08:15:13 +0900

    @atoms 下のコンポーネントをグローバル化

--- src/main.js
+++ src/main.js
@@ -2,11 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-
 import './assets/sass/style.scss'
+
+const app = createApp(App)
+app.use(vuetify)
 
 loadFonts()
 
-createApp(App)
-  .use(vuetify)
-  .mount('#app')
+// @atoms 以下のコンポーネントをグローバルで登録
+const components = import.meta.globEager('./components/atoms/*.vue')
+Object.entries(components)
+  .map(([path, { default: component }]) => [
+    path.match(/[^/]+(?=\.vue)/)[0],
+    component,
+  ])
+  .forEach(([name, component]) =>
+    app.component(name, component, console.log({ name, component }))
+  )
+
+app.mount('#app')
