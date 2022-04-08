9de74c55395e6211f15b86cf828a4bffce20a709
482F <0ryonryoko0@gmail.com>
2022-04-09 08:59:38 +0900

    textarea コンポーネントを追加

---
+++ src/components/atoms/a-textarea.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-textarea class="a-textarea" variant="outlined" no-resize />
+</template>
+
+<script>
+export default {
+  name: 'a-textarea',
+}
+</script>
+
+<style lang="scss" scoped>
+.a-textarea {
+  display: block;
+  > ::v-deep(.v-input__control) {
+    height: 100%;
+    > .v-field {
+      height: 100%;
+    }
+  }
+  > ::v-deep(.v-input__details) {
+    display: none;
+  }
+}
+</style>
