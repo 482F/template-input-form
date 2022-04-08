7600436bee21ce9dfa6b83612659a9b2b3f1427f
482F <0ryonryoko0@gmail.com>
2022-04-09 06:55:33 +0900

    スクロールバーのスタイル修正

--- src/App.vue
+++ src/App.vue
@@ -25,5 +25,16 @@ export default {
 <style lang="scss">
 * {
   font-family: 'Cica';
+  &::-webkit-scrollbar-track {
+    background-color: #eee;
+  }
+  &::-webkit-scrollbar {
+    width: 10px;
+    height: 10px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: #ccc;
+    border-radius: 30px;
+  }
 }
 </style>
