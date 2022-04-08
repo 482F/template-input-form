bc55bf8c2cf9e5493035b37d7d5d7e49715a9372
482F <0ryonryoko0@gmail.com>
2022-04-09 08:22:29 +0900

    button, icon, select コンポーネントを追加

---
+++ src/components/atoms/a-button.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-btn
+    @click="$emit('click', $event)"
+    :icon="icon"
+    flat
+    density="compact"
+    :variant="variant"
+  >
+    <slot />
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'a-button',
+  props: {
+    icon: {
+      type: Boolean,
+      default: false,
+    },
+    variant: {
+      type: String,
+      default: 'contained',
+    },
+  },
+}
+</script>
---
+++ src/components/atoms/a-select.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-select
+    :items="items"
+    class="a-select"
+    density="compact"
+    variant="outlined"
+  />
+</template>
+
+<script>
+export default {
+  name: 'a-select',
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.a-select {
+  > ::v-deep(.v-input__details) {
+    display: none;
+  }
+}
+</style>
