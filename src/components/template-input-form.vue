3cfa15bb91edd9fd8a2bf52ab0ac4083472b42ca
482F <0ryonryoko0@gmail.com>
2022-04-09 11:41:01 +0900

    template からオブジェクト部を切り出せるように

--- src/components/template-input-form.vue
+++ src/components/template-input-form.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="template-input-form">
-    <div v-for="(line, i) of separatedBody" :key="i">
-      {{ line }}
-    </div>
+    <span v-for="(object, i) of separatedBody" :key="i">
+      <show-object :object="object" />
+    </span>
   </div>
 </template>
 
 <script>
+import ShowObject from './show-object.vue'
+
+function parseObject(objectString) {
+  return Object.fromEntries(
+    [
+      ...objectString
+        .replaceAll(/^\$\{|\}$/gm, '')
+        .matchAll(/(?<key>[^\s]+?):\s+(?<value>(\[[^\]]+\]|[^,]+))(,|$)/g),
+    ].map((match) => [match.groups.key, match.groups.value])
+  )
+}
+
 export default {
   name: 'template-input-form',
+  components: {
+    ShowObject,
+  },
   props: {
     template: {
       type: Object,
@@ -22,15 +37,36 @@ export default {
   computed: {
     separatedBody() {
       const body = this.template?.body
+      let objectIndex = -1
+      const objects = []
       if (!body) {
         return []
       }
       const separatedBody = []
+
       this.template.body
-        .replaceAll(/\/\*(.|\n)+?\*\/|\/\/.+?\n/gm, '')
+        .replaceAll(/\/\*(.|\n)+?\*\/|\/\/.+?\n/g, '') // コメント (// foo, /* bar */)
+        .replaceAll(/\$\{([^}]+)\}/g, (match) => {
+          // オブジェクト (${foobar})
+          // 副作用有り
+          objectIndex += 1
+          objects[objectIndex] = parseObject(match)
+          return `\${${objectIndex}}`
+        })
         .split('\n')
         .forEach((line) => {
-          separatedBody.push({ type: 'text', value: line })
+          const matches = line.matchAll(/\$\{(\d+)\}/g)
+          let processed = 0
+          const pushTextIfNeeded = (text) =>
+            text ? separatedBody.push({ type: 'text', value: text }) : undefined
+
+          for (const match of matches) {
+            pushTextIfNeeded(line.slice(processed, match.index))
+            separatedBody.push(objects[match[1]])
+            processed += match.index + match[0].length
+          }
+          pushTextIfNeeded(line.slice(processed, line.length))
+          separatedBody.push({ type: 'newline' })
         })
       return separatedBody
     },
