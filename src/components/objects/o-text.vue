4b5ee0c0c257f5968db426ff9983bc2519070e93
482F <0ryonryoko0@gmail.com>
2022-04-09 13:27:55 +0900

    入力結果を受け取れるように

--- src/components/objects/o-text.vue
+++ src/components/objects/o-text.vue
@@ -10,6 +10,9 @@ import ObjectsMixin from './objects.mixin.js'
 export default {
   name: 'o-text',
   mixins: [ObjectsMixin],
+  mounted() {
+    this.$emit('update:result', { body: this.object.value })
+  },
 }
 </script>
 
