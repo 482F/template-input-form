d97124bdbb4e4fde67571f931417fe7f980b5fca
482F <0ryonryoko0@gmail.com>
2022-04-09 18:40:15 +0900

    type: switch を追加

---
+++ src/components/atoms/a-switch.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-switch
+    class="a-switch"
+    ref="root"
+    flat
+    density="compact"
+    color="blue"
+    :label="label"
+  />
+</template>
+
+<script>
+export default {
+  name: 'a-switch',
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    focus() {
+      this.$refs.root?.$el?.getElementsByTagName('input')?.[0]?.focus?.[0]
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.a-switch {
+  padding: 0 1rem;
+  > ::v-deep(.v-input__details) {
+    display: none;
+  }
+}
+</style>
