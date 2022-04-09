c784355860f11bafa31fd9b0f8cc489845743d76
482F <482F.git.mail@482f.net>
2022-04-10 00:00:00 +0900

    文章入力用の type: sentence を追加

---
+++ src/components/objects/o-sentence.vue
@@ -0,0 +1,41 @@
+<template>
+  <o-list
+    ref="oList"
+    :auto-indent="false"
+    :object="{
+      heads: '[]',
+      indent: '  ',
+      default: object?.default,
+    }"
+  />
+</template>
+
+<script>
+import OList from './o-list.vue'
+import ObjectsMixin from './objects.mixin.js'
+
+export default {
+  name: 'o-sentence',
+  components: {
+    OList,
+  },
+  mixins: [ObjectsMixin],
+  data() {
+    return {
+      value: '',
+    }
+  },
+  methods: {
+    focus() {
+      this.$refs.oList.focus()
+    },
+    update(newValue) {
+      this.value = newValue
+      this.$emit('update:result', { body: newValue })
+    },
+  },
+  mounted() {
+    this.update(this.object.default ?? '')
+  },
+}
+</script>
